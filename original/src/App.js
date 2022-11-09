e99525dbf3a4eb13c690cdc9e55f07461cc5a22c
Viktor Ohád <viktor.ohad@protonmail.com>
2022-11-09 09:55:12 +0100

    Lodash with named import

--- original/src/App.js
+++ original/src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { InstUISettingsProvider, View } from "@instructure/ui";
-import clone from "lodash/clone";
+import { clone } from "lodash";
 
 function App() {
   const c = clone([1, 2, 3]);
